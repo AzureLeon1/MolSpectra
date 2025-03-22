c7361e0e1b7f6ebdf50979147e6bc8aa18c5c6e5
Liang Wang <wangliang.leon20@gmail.com>
2025-03-22 13:38:26 +0800

    update

--- torchmdnet/datasets/__init__.py
+++ torchmdnet/datasets/__init__.py
@@ -5,7 +5,6 @@ from .ani1 import ANI1
 from .custom import Custom
 from .hdf import HDF5
 from .pcqm4mv2 import PCQM4MV2_XYZ as PCQM4MV2
-from .multi_spec import MultiSpec, MultiSpecLarge
 
 __all__ = [
     "QM9",
